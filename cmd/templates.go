36735a06adbbe6da5886b94122d4cf24c7c2e07e
alexrefshauge <nk02hb@aau.student.dk>
2024-12-01 12:05:34 +0100

    templates

--- cmd/templates.go
+++ cmd/templates.go
@@ -3,10 +3,44 @@ package cmd
 const (
 	solutionFile = `package main
 
-import "fmt"
+import (
+	_ "embed"
+	"flag"
+	"fmt"
+	"os"
+)
+
+//go:embed input.txt
+var input string
 
 func main() {
-	fmt.Printf("Running AoC day %%d solution\n", %[1]d)
+	var part int
+	flag.IntVar(&part, "part", 1, "part 1 or 2")
+	flag.Parse()
+	fmt.Printf("Running AoC day %%d solution (part %%d)\n", %[1]d, part)
+
+	var answer string
+	if part == 1 {
+		answer = part1(input)
+	} else if part == 2 {
+		answer = part2(input)
+	} else {
+		panic(fmt.Errorf("unable to run part %%d", part))	
+	}
+
+	fmt.Println("Output:", answer)
+	err := os.WriteFile(fmt.Sprintf("output%%d.txt", part), []byte(answer), 0644)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func part1(input string) string {
+	return input
+}
+
+func part2(input string) string {
+	return input
 }
 `
 
